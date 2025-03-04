4911777590b53030a324f015cead4a0e61f8180f
Henry Leon <helg18@gmail.com>
2025-03-03 23:58:55 -0400

    Creando el Servicio para consumir la API

---
+++ src/Services/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://randomfox.ca/floof/"; // Ejemplo de URL base
+const request = axios.create({
+    baseURL: BASE_URL,
+});
+
+class Api {
+    getNewDog = async () => request.get()
+        .then(function (response) {
+            // handle success
+            return response.data;
+        });
+}
+
+export default new Api();
